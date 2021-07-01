d7a34969e2178a31ae0d739c4db9ffbf831ad1dd
psulli10 <petersullivan1985@gmail.com>
2021-07-01 23:15:59 +0100

    export default added for dummy data files

---
+++ src/data/homeData.js
@@ -0,0 +1,16 @@
+const homeData = [
+    {
+        url: 'https://source.unsplash.com/200x300/?coder',
+        text: null
+    },
+    {
+        url: null,
+        text: "Some test text about Peter Sullivan"
+    },
+    {
+        url: 'https://source.unsplash.com/200x300/?coder',
+        text: null
+    }
+]
+
+export default homeData;
